aeb2bdd86e71b13148102d9ab58d24c950f982e9
Fer.Tapia <fer.tapia.vil@gmail.com>
2020-11-06 20:47:15 -0500

    iNEXT

---
+++ Rscripts/Curvas_acumulación_iNEXT.R
@@ -0,0 +1,9 @@
+##################### CURVAS DE RAREFACIÓN Y EXTRAPOLACIÓN BASADAS EN NÚMEROS DE HILL #################
+
+# FUENTES: 
+# 1. T. C. Hsieh  K. H. Ma  Anne Chao (2016). iNEXT: an R package for rarefaction and extrapolation of species diversity (Hill numbers) 
+#  https://doi.org/10.1111/2041-210X.12613
+
+# 2. Appendix iNEXT: An R package for rarefaction and extrapolation of species diversity (Hill numbers)
+# https://besjournals.onlinelibrary.wiley.com/action/downloadSupplement?doi=10.1111%2F2041-210X.12613&file=mee312613-sup-0001-AppendixS1.pdf
+
